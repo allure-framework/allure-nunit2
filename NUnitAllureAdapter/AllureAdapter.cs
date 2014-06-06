118f634c7163835e8b633ea4c39ef3b3e1a53d1f
ilya-murzinov <murz42@gmail.com>
2014-06-06 12:59:50 +0400

    add stubs

---
+++ NUnitAllureAdapter/AllureAdapter.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Core.Extensibility;
+
+namespace NUnitAllureAdapter
+{
+    [NUnitAddin(Name = "Allure adapter", Type = ExtensionType.Core)]
+    public class AllureAdapter : IAddin
+    {
+        public bool Install(IExtensionHost host)
+        {
+            IExtensionPoint listeners = host.GetExtensionPoint("EventListeners");
+            listeners.Install(new AllureEventListener());
+            return true;
+        }
+    }
+}
---
+++ NUnitAllureAdapter/AllureEventListener.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Core;
+
+namespace NUnitAllureAdapter
+{
+    internal class AllureEventListener : EventListener
+    {
+        public void RunStarted(string name, int testCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunFinished(TestResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunFinished(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestStarted(TestName testName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestFinished(TestResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SuiteStarted(TestName testName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SuiteFinished(TestResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnhandledException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestOutput(TestOutput testOutput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
