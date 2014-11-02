9a8daa8a64770081893c9b2a2d41a2bd6a7fa1a9
Ilya Murzinov <murz42@gmail.com>
2014-11-02 23:55:00 +0400

    fix missing file

---
+++ NUnitAllureAdapter/EventListenerAdapter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using log4net;
+using NUnit.Core;
+
+namespace NUnitAllureAdapter
+{
+    public class EventListenerAdapter : EventListener
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof (EventListenerAdapter));
+
+        public virtual void RunStarted(string name, int testCount)
+        {
+        }
+
+        public virtual void RunFinished(TestResult result)
+        {
+        }
+
+        public virtual void RunFinished(Exception exception)
+        {
+        }
+
+        public virtual void TestStarted(TestName testName)
+        {
+        }
+
+        public virtual void TestFinished(TestResult result)
+        {
+        }
+
+        public virtual void SuiteStarted(TestName testName)
+        {
+        }
+
+        public virtual void SuiteFinished(TestResult result)
+        {
+        }
+
+        public virtual void UnhandledException(Exception exception)
+        {
+            Logger.Error(String.Format("UnhandledException"), exception);
+        }
+
+        public virtual void TestOutput(TestOutput testOutput)
+        {
+        }
+    }
+}
